1783e870336abb20ae0d36ac1f1493688226ca78
gyq765 <kenji.gohyiqian@gmail.com>
2021-11-03 16:15:50 +0800

    Added Login, Product and Cart Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-// import Newsletter from "../components/Newsletter";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
@@ -15,8 +15,8 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
-      {/* <Newsletter />
-      <Footer /> */}
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
